994eba1fc3ca113e97d60870bf1561201e125527
marekar <marqusp@gmail.com>
2017-11-13 18:23:14 +0100

    drugi

--- taboo search/main.cpp
+++ taboo search/main.cpp
@@ -1,28 +1,37 @@
 #include "worker.h"
 #include <fstream>
 
-//const char * file_name_worker = "worker_data.txt";
+#define workers_amount 3
 
-bool read_worker_data();
+const char * file_name_worker = "worker_data.txt";
+
+bool read_worker_data(Worker **workers);
 ifstream file;
 
 int main ()
 {
-    Worker pierwszy = Worker(1, 4, 5, 6);
-    pierwszy.print_worker_data();
-    cout << pierwszy.skills[2] <<endl;
-    
-    if(!read_worker_data())
+    //Worker pierwszy = Worker(1, 4, 5, 6);
+    //pierwszy.print_worker_data();
+    //cout << pierwszy.skills[2] <<endl;
+
+    Worker **workers;
+    workers = new *Worker[workers_amount];
+    for(int i = 0; i < workers_amount; i++)
+    {
+        workers[i] = new Worker();
+    }
+
+    if(!read_worker_data(workers))
         cout << "Wystapil blad podczas pobierania danych " << endl;
     
 
-    pierwszy.print_worker_data();
+    workers[0] -> print_worker_data();
     return 0;
 }
 
-bool read_worker_data()
+bool read_worker_data(Worker **workers)
 {
-    file.open("worker_data.txt");
+    file.open(file_name_worker);
     if(!file.good())
     {
         cout << "Nie mozna wczytac pliku worker_data.txt" << endl;
@@ -31,14 +40,20 @@ bool read_worker_data()
   
     while(true)
     {
-        int skill_tab[3];
+        int k = amount_of_skills;
+        int i = 0;
+        int skill_tab[9];
         file >> skill_tab[0] >> skill_tab[1] >> skill_tab[2];
         if(file.good())
         {
-            for(int i = 0; i < amount_of_skills; i++)
+            for(int j = 0; j < workers_amount; j++)
             {
-                cout << skill_tab[i] << endl;
-                pierwszy.skills[i] = skill_tab[i];
+                for(int i; i < k; i++)
+                {
+                    workers[j] -> skills[i] = skill_tab[i];
+                }
+                i += amount_of_skills;
+                k += k;
             }
         }
         else
