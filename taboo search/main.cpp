eccf0c28db0e2cc57e645f45d2617d4884b39949
iThom96 <tomasz.lisek96@gmail.com>
2017-11-13 18:37:06 +0100

    Działa

--- taboo search/main.cpp
+++ taboo search/main.cpp
@@ -14,8 +14,7 @@ int main ()
     //pierwszy.print_worker_data();
     //cout << pierwszy.skills[2] <<endl;
 
-    Worker **workers;
-    workers = new *Worker[workers_amount];
+    Worker* workers[workers_amount];
     for(int i = 0; i < workers_amount; i++)
     {
         workers[i] = new Worker();
@@ -23,7 +22,7 @@ int main ()
 
     if(!read_worker_data(workers))
         cout << "Wystapil blad podczas pobierania danych " << endl;
-    
+
 
     workers[0] -> print_worker_data();
     return 0;
@@ -37,7 +36,7 @@ bool read_worker_data(Worker **workers)
         cout << "Nie mozna wczytac pliku worker_data.txt" << endl;
         return false;
     }
-  
+
     while(true)
     {
         int k = amount_of_skills;
@@ -48,7 +47,7 @@ bool read_worker_data(Worker **workers)
         {
             for(int j = 0; j < workers_amount; j++)
             {
-                for(int i; i < k; i++)
+                for(; i < k; i++)
                 {
                     workers[j] -> skills[i] = skill_tab[i];
                 }
