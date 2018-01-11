32862a43bb90b48e6cc469e7a8425c5ffd86dcd1
marekar <marek.piekarz@gmail.com>
2018-01-11 12:31:01 +0100

    dokumentacja + kolory

--- taboo search/chartgraph.m
+++ taboo search/chartgraph.m
@@ -3,7 +3,7 @@
 close all;
 clear all;
 
-output = importdata('output.csv');
+output = importdata('output2.csv');
 [rows, cols] = size(output.data);
 workers_amount = (rows)/2; %iloœæ pracowników
 
@@ -28,42 +28,73 @@ xlabel('time');
 set(gca, 'YDir', 'reverse', 'YTickLabel', workers);
 color = [1 1 1];
 
+
+
 for j = 1:cols-1
     H = solution(:,cols - j);
     for i = 1:N
         hold on;
         h = barh(i, H(i));
 
-        %Okreslono jedynie 12 kolorów dla problemów, trzeba bêdzie to
-        %zrobiæ w pêtli zale¿nej od iloœci problemów. 
         if tasks(i, cols - j) == 0
             color = [1 1 1];
         elseif tasks(i, cols - j) == 1
-            color = [1 1 0];
-        elseif tasks(i, cols - j) == 2
-            color = [0.3 0.5 0.6];
-        elseif tasks(i, cols - j) == 3
-            color = [0 1 1];
-        elseif tasks(i, cols - j) == 4
             color = [1 0 0];
+        elseif tasks(i, cols - j) == 2
+            color = [0.7 0 0];
+        elseif tasks(i, cols - j) == 3
+            color = [0.4 0 0 ];
+        elseif tasks(i, cols - j) == 4
+            color = [0 1 0];
         elseif tasks(i, cols - j) == 5
-            color = [0 1 0];               
+            color = [0 0.7 0];               
         elseif tasks(i, cols - j) == 6
-            color = [0 0 1];   
+            color = [0 0.4 0];   
         elseif tasks(i, cols - j) == 7
-            color = [0 0 0];   
+            color = [0 0 1];   
         elseif tasks(i, cols - j) == 8
-            color = [0 0.5 0];   
+            color = [0 0 0.7];   
         elseif tasks(i, cols - j) == 9
-            color = [0.5 0 0];   
+            color = [0 0 0.4];   
         elseif tasks(i, cols - j) == 10
-            color = [0 0.3 0.6];   
+            color = [0 0.7 0.7];   
         elseif tasks(i, cols - j) == 11
-            color = [0.5 0.5 0];   
+            color = [0 0.4 0.4];   
         elseif tasks(i, cols - j) == 12
-            color = [0.5 0 0.5];   
+            color = [0 1 1];   
+        elseif tasks(i, cols - j) == 13
+            color = [1 1 0];
+        elseif tasks(i, cols - j) == 14
+            color = [0.7 0.7 0];
+        elseif tasks(i, cols - j) == 15
+            color = [0.4 0.4 0 ];
+        elseif tasks(i, cols - j) == 16
+            color = [1 0 1];
+        elseif tasks(i, cols - j) == 17
+            color = [0.7 0 0.7];               
+        elseif tasks(i, cols - j) == 18
+            color = [0.4 0 0.4];   
+        elseif tasks(i, cols - j) == 19
+            color = [0.7 0.7 0.7];   
+        elseif tasks(i, cols - j) == 20
+            color = [0.4 0.4 0.4];   
+        elseif tasks(i, cols - j) == 21
+            color = [1 0 0.4];   
+        elseif tasks(i, cols - j) == 22
+            color = [0.2 0.7 0.7];   
+        elseif tasks(i, cols - j) == 23
+            color = [0 1 0.4];   
+        elseif tasks(i, cols - j) == 24
+            color = [0.3 0.4 0.5];   
         end
-       set(h, 'FaceColor', color,'EdgeColor', [1 1 1], 'LineWidth', 0.1);
+        
+        
+        
+        %Okreslono jedynie 12 kolorów dla problemów, trzeba bêdzie to
+        %zrobiæ w pêtli zale¿nej od iloœci problemów. 
+        
+        
+        set(h, 'FaceColor', color,'EdgeColor', [1 1 1], 'LineWidth', 0.1);
     end
    
 end
